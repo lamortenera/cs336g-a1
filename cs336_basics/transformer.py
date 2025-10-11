Add correct masking for eos tokens

cs336_basics/transformer.py
@@ -129,20 +129,18 @@ class MultiHeadSelfAttention(torch.nn.Module):
         self.weights_qkv = get_weights((3*d_model, d_model), np.sqrt(2/(d_model + d_model)), device=device, dtype=dtype)
         self.weights_o = get_weights((d_model, d_model), np.sqrt(2/(d_model + d_model)), device=device, dtype=dtype)
         
-        mask = torch.ones(max_seq_len, max_seq_len).tril() > 0
-        self.register_buffer("mask", mask, persistent=False)
-
         self.dtype= self.weights_qkv.dtype
         self.num_heads = num_heads
 
 
     def forward(self, x: Float[torch.Tensor, "... seq_len d_model"], 
+                mask: Bool[torch.Tensor, "... seq_len seq_len"],
                 token_positions: Optional[Float[torch.Tensor, "... seq_len"]]=None):
         orig_dtype = x.dtype
         x = x.to(self.dtype)
         QKV = einsum(x, self.weights_qkv, 
         "... seq_len d_model_in, d_model_out_3x d_model_in -> ... seq_len d_model_out_3x")
-        QKV = rearrange(QKV, "... seq_len (three num_heads d_k)-> three ... num_heads seq_len d_k", 
+        QKV = rearrange(QKV, "... seq_len (three num_heads d_k)-> three num_heads ... seq_len d_k", 
                         num_heads=self.num_heads, three=3)
         
         seq_len = x.shape[-2]
@@ -155,10 +153,10 @@ class MultiHeadSelfAttention(torch.nn.Module):
                 Q = self.rope.forward(Q, seq_len)
                 K = self.rope.forward(K, seq_len)
 
-        O = scaled_dot_product_attention(Q, K, V, self.mask[:seq_len,:seq_len])
+        O = scaled_dot_product_attention(Q, K, V, mask)
 
 
-        O = rearrange(O, "... num_heads seq_len d_k -> ... seq_len (num_heads d_k)")
+        O = rearrange(O, "num_heads ... seq_len d_k -> ... seq_len (num_heads d_k)")
         result = einsum(O, self.weights_o, "... seq_len d_model_in, d_model_out d_model_in -> ... seq_len d_model_out")
         return result.to(orig_dtype)
 
@@ -172,18 +170,30 @@ class TransformerBlock(torch.nn.Module):
         self.ff_norm = RMSNorm(d_model, device=device, dtype=dtype)
         self.ff = SwiGLU(d_model, d_ff, device=device, dtype=dtype)
 
-    def forward(self, x: Float[torch.Tensor, "... seq_len d_model"]):
+    def forward(self, x: Float[torch.Tensor, "... seq_len d_model"], mask: Bool[torch.Tensor, "... seq_len seq_len"]):
         x_norm = self.mha_norm.forward(x)
-        x_mha = self.mha.forward(x_norm)
+        x_mha = self.mha.forward(x_norm, mask)
         x2 = x + x_mha
 
         x2_norm = self.ff_norm.forward(x2)
         x2_ff = self.ff.forward(x2_norm)
         return x2 + x2_ff
 
+def get_attention_mask(tokens: Int[torch.Tensor, "... seq_len"], 
+                       eos_token: Optional[int]=0) -> Bool[torch.Tensor, "... seq_len seq_len"]:
+    seq = torch.arange(tokens.shape[-1], device=tokens.device)
+    max_token_for_attention = seq[:, None]
+    if eos_token is None:
+        return seq <= max_token_for_attention
+
+    prev_token_is_eos = torch.roll(tokens == eos_token, 1, dims=-1)
+    min_token_for_attention = torch.cummax(prev_token_is_eos * seq, dim=-1).values[..., None]
+    return (seq >= min_token_for_attention) & (seq <= max_token_for_attention)
+
 class TransformerLM(torch.nn.Module):
     def __init__(self, *, vocab_size: int, num_layers: int, d_model: int, num_heads: int, 
-                 d_ff: int, max_seq_len: int, theta: Optional[float], device=None, dtype=None, attention_dtype=None):
+                 d_ff: int, max_seq_len: int, 
+                 theta: Optional[float], eos_token: int = 0, device=None, dtype=None, attention_dtype=None):
         super().__init__()
         self.embedding = Embedding(vocab_size, d_model, device=device, dtype=dtype)
         self.layers = torch.nn.ModuleList([])
@@ -192,17 +202,15 @@ class TransformerLM(torch.nn.Module):
                     TransformerBlock(d_model, num_heads, d_ff, max_seq_len, theta, device=device, dtype=dtype, attention_dtype=attention_dtype))
         self.final_norm = RMSNorm(d_model, device=device, dtype=dtype)
         self.output_proj = Linear(d_model, vocab_size, device=device, dtype=dtype)
+        self.eos_token = eos_token
 
     def forward(self, indices: Int[torch.Tensor, "... seq_len"]) -> Float[torch.Tensor, "... seq_len vocab_size"]:
+        mask = get_attention_mask(indices, self.eos_token)
         x = self.embedding.forward(indices)
         for layer in self.layers:
-            x = layer.forward(x)
+            x = layer.forward(x, mask)
         x = self.final_norm.forward(x)
         x = self.output_proj.forward(x)
         return x
 
-         
         
-
-        
-

tests/adapters.py
@@ -171,7 +171,9 @@ def run_multihead_self_attention(
        "weights_qkv": torch.cat((q_proj_weight,k_proj_weight,v_proj_weight), axis=0),
        "weights_o": o_proj_weight
        })
-    return multihead_self_attention.forward(in_features, None)
+    seq = torch.arange(in_features.shape[-2])
+    attention_mask = seq <= seq[:, None]
+    return multihead_self_attention.forward(in_features, attention_mask)
     # t2 = torch.nn.MultiheadAttention(d_model, num_heads, bias=False, batch_first=True, 
     #                                  device=in_features.device, dtype=in_features.dtype)
     # t2.load_state_dict({
@@ -228,8 +230,13 @@ def run_multihead_self_attention_with_rope(
        "weights_qkv": torch.cat((q_proj_weight,k_proj_weight,v_proj_weight), axis=0),
        "weights_o": o_proj_weight
        })
-    return multihead_self_attention.forward(in_features, token_positions)
+    seq = torch.arange(in_features.shape[-2])
+    attention_mask = seq <= seq[:, None]
+    return multihead_self_attention.forward(in_features, attention_mask, token_positions)
 
+
+def run_get_attention_mask(tokens: Int[Tensor, "... sequence_length"], eos_token: int = 0):
+    return transformer.get_attention_mask(tokens, eos_token)
 
 def run_rope(
     d_k: int,

tests/test_model.py
@@ -15,6 +15,7 @@ from .adapters import (
     run_transformer_lm,
     run_linear,
     run_embedding,
+    run_get_attention_mask
 )
 
 
@@ -41,7 +42,8 @@ def test_embedding(numpy_snapshot, ts_state_dict, in_indices, vocab_size, d_mode
 
 
 def test_swiglu(numpy_snapshot, ts_state_dict, in_embeddings, d_model, d_ff):
-    w1_weight, w2_weight, w3_weight = [ts_state_dict[0][f"layers.0.ffn.{k}.weight"] for k in ["w1", "w2", "w3"]]
+    w1_weight, w2_weight, w3_weight = [
+        ts_state_dict[0][f"layers.0.ffn.{k}.weight"] for k in ["w1", "w2", "w3"]]
 
     actual_output = run_swiglu(
         d_model=d_model,
@@ -64,8 +66,10 @@ def test_scaled_dot_product_attention(numpy_snapshot, q, k, v, mask):
 
 def test_4d_scaled_dot_product_attention(numpy_snapshot, q, k, v, mask):
     # Shape: (batch_size, num_heads, seq_len, d_k)
-    q, k, v = (rearrange(x, "(batch head) seq d -> batch head seq d", head=2) for x in (q, k, v))
-    mask = rearrange(mask, "(batch head) query key -> batch head query key", head=2)
+    q, k, v = (rearrange(x, "(batch head) seq d -> batch head seq d", head=2)
+               for x in (q, k, v))
+    mask = rearrange(
+        mask, "(batch head) query key -> batch head query key", head=2)
 
     actual_output = run_scaled_dot_product_attention(Q=q, K=k, V=v, mask=mask)
     numpy_snapshot.assert_match(
@@ -156,7 +160,8 @@ def test_transformer_lm_truncated_input(
 
 
 def test_transformer_block(numpy_snapshot, ts_state_dict, in_embeddings, d_model, n_heads, d_ff, n_keys, theta):
-    block_weights = {k.replace("layers.0.", ""): v for k, v in ts_state_dict[0].items() if "layers.0." in k}
+    block_weights = {k.replace("layers.0.", ""): v for k,
+                     v in ts_state_dict[0].items() if "layers.0." in k}
 
     actual_output = run_transformer_block(
         d_model=d_model,
@@ -178,7 +183,8 @@ def test_rmsnorm(numpy_snapshot, ts_state_dict, in_embeddings):
     reference_weights = state_dict["layers.1.ln1.weight"]
     d_model = reference_weights.shape[0]
 
-    actual_output = run_rmsnorm(d_model=d_model, eps=1e-5, weights=reference_weights, in_features=in_embeddings)
+    actual_output = run_rmsnorm(
+        d_model=d_model, eps=1e-5, weights=reference_weights, in_features=in_embeddings)
 
     numpy_snapshot.assert_match(actual_output, atol=1e-6)
 
@@ -199,4 +205,72 @@ def test_silu_matches_pytorch():
     )
     expected_output = F.silu(x)
     actual_output = run_silu(x)
-    numpy.testing.assert_allclose(actual_output.detach().numpy(), expected_output.detach().numpy(), atol=1e-6)
+    numpy.testing.assert_allclose(actual_output.detach(
+    ).numpy(), expected_output.detach().numpy(), atol=1e-6)
+
+
+def test_get_attention_mask():
+    numpy.testing.assert_equal(run_get_attention_mask(torch.tensor([1, 2, 3])).detach().numpy(),
+                               numpy.array([[True, False, False],
+                                            [True, True, False],
+                                            [True, True, True],
+                                            ])
+                               )
+    numpy.testing.assert_equal(run_get_attention_mask(torch.tensor([[[1, 2, 3],
+                                                                     [4, 5, 6]],
+                                                                    [[3, 2, 1],
+                                                                     [6, 5, 4]],
+                                                                    ])).detach().numpy(),
+                               numpy.array(
+                                   [[[
+                                       [True, False, False],
+                                       [True, True, False],
+                                       [True, True, True],
+                                   ],
+                                       [
+                                       [True, False, False],
+                                       [True, True, False],
+                                       [True, True, True],
+                                   ]],
+                                       [[
+                                           [True, False, False],
+                                           [True, True, False],
+                                           [True, True, True],
+                                       ],
+                                       [
+                                           [True, False, False],
+                                           [True, True, False],
+                                           [True, True, True],
+                                       ]],
+                                   ])
+                               )
+
+    numpy.testing.assert_equal(run_get_attention_mask(torch.tensor([1, 2, 3, 0, 4, 5, 0, 6, 7])).detach().numpy(),
+                               numpy.array([[True, False, False, False, False, False, False, False, False],  # nopep8
+                                            [True, True, False, False, False, False, False, False, False],  # nopep8
+                                            [True, True, True, False, False, False, False, False, False],  # nopep8
+                                            [True, True, True, True, False, False, False, False, False],  # nopep8
+                                            [False, False, False, False, True, False, False, False, False],  # nopep8
+                                            [False, False, False, False, True, True, False, False, False],  # nopep8
+                                            [False, False, False, False, True, True, True, False, False],  # nopep8
+                                            [False, False, False, False, False, False, False, True, False],  # nopep8
+                                            [False, False, False, False, False, False, False, True, True]  # nopep8
+                                            ])
+                               )
+
+    numpy.testing.assert_equal(run_get_attention_mask(torch.tensor([[0, 2, 3],
+                                                                    [1, 0, 3],
+                                                                    [1, 2, 0],
+                                                                    ])).detach().numpy(),
+                               numpy.array([[[True, False, False],
+                                            [False, True, False],
+                                            [False, True, True],
+                                             ],
+                                            [[True, False, False],
+                                            [True, True, False],
+                                            [False, False, True],
+                                             ],
+                                            [[True, False, False],
+                                            [True, True, False],
+                                            [True, True, True],
+                                             ]]))